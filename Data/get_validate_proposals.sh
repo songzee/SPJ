46b5a72b4f8726ccb0a0cba9b931224c6c1ebc68
Julio Martinez <juliomz@stanford.edu>
2018-04-28 18:52:36 +0000

    json reading, csv conversion, proposal module

---
+++ Data/get_validate_proposals.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# ```
+#
+# Sample bash-script used to read all video ids from csv files
+# and run Proposal Module to return proposals into csv files
+#
+#
+# Usage: ./get_proposals.sh OR sh get_proposals.sh
+# Requirements: conda
+#
+# ```
+
+DIR="../../daps"
+MODEL="T512K64_thumos14.npz"
+while read VideoID; do
+  echo "VideoID: $VideoID"
+  $DIR/tools/generate_proposals.py -iv $VideoID -ic3d sub_activitynet_v1-3.c3d.hdf5 -imd $DIR/$MODEL
+  echo "\n"
+done <validate_ids.csv
