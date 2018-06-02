16fc4091bd2e3e302d0ab4ac6678cd166a7a2784
Julio Martinez <juliomz@stanford.edu>
2018-06-02 01:35:28 +0000

    concatenate Hout to each time step in x for lstm

--- utils/spj.py
+++ utils/spj.py
@@ -15,7 +15,7 @@ class Config(object):
     num_proposals = 30
     num_classes = 10194
     num_steps = 50
-    batch_size = 32
+    batch_size = 1
     hidden_dim = 512
     num_layers = 1
     eps = 1e-10
@@ -58,26 +58,6 @@ class SPJ(object):
         Hout = tf.concat([Hpast, self._H, Hfuture], 1)
         # End Attention Module
         # -----------------------
-        
-        #Wa = tf.get_variable("Wa",[self.config.num_c3d_features,self.config.num_c3d_features],initializer=tf.contrib.layers.xavier_initializer(seed=1))
-        #ba = tf.get_variable("ba", [self.config.num_c3d_features, 1], initializer = tf.zeros_initializer())
-
-        # Forward Pass
-        #W = tf.transpose(tf.tensordot(Wa,tf.transpose(self._H,perm=[1,2,0]),axes=[[1], [0]]),perm=[2,0,1]) + ba # shape: [None,num_proposals,num_proposals]
-        #A = tf.matmul(tf.transpose(W,perm=[0,2,1]),self._H) # shape: [None,num_proposals,num_proposals]
-        #A_flat = tf.reshape(A, [-1, self.config.num_proposals*self.config.num_proposals]) 
-
-        # Future Features
-        #Ifuture_flat = tf.reshape(self._Ifuture, [-1, self.config.num_proposals*self.config.num_proposals]) # shape: [None,K*K]
-        #Afuture = tf.reshape(tf.multiply(Ifuture_flat,A_flat),[-1, self.config.num_proposals, self.config.num_proposals]) # shape: [None,K,K]
-        #Zfuture = tf.reduce_sum(self._Ifuture,axis=2) + self.config.eps # shape: [None,num_proposals]
-        #Hfuture = tf.transpose(tf.transpose(tf.matmul(self._H,tf.transpose(Afuture,perm=[0,2,1])),perm=[1,0,2])/Zfuture,perm=[1,0,2]) # shape: [None,num_c3d_features,num_proposals]
-
-        # Past Features
-        #Ipast_flat = tf.reshape(self._Ipast, [-1, self.config.num_proposals*self.config.num_proposals]) # shape: [None,num_proposals*num_proposals]
-        #Apast = tf.reshape(tf.multiply(Ipast_flat,A_flat),[-1,self.config.num_proposals, self.config.num_proposals]) # shape: [None,num_proposals,num_proposals]
-        #Zpast = tf.reduce_sum(self._Ipast, axis=2) + self.config.eps # shape: [None,num_proposals]
-        #Hpast = tf.transpose(tf.transpose(tf.matmul(self._H,tf.transpose(Apast,perm=[0,2,1])),perm=[1,0,2])/Zfuture,perm=[1,0,2]) # shape: [None,num_c3d_features,num_proposals]
 
         # Stacked Features
         #Hout = tf.concat([Hpast, self._H, Hfuture], 1)
@@ -89,32 +69,30 @@ class SPJ(object):
         
         # Begin Captioning Module
         # -----------------------
-        
         Hout = tf.transpose(Hout, perm=[0,2,1])
         Hout = tf.reshape(Hout, [-1, 3*self.config.num_c3d_features]) 
 
-        # FC Layer from num_c3d_features to hidden_dim
+        # FC Layer from num_c3d_features to hidden_dim, feature_inputs.shape=[-1, 1, 512]
         feature_inputs = tf.expand_dims(tf.layers.dense(inputs=Hout,units=self.config.hidden_dim,activation=tf.nn.relu),1) 
 
-        # Trainable Word Embeddings
+        # Trainable Word Embeddings, embedding_inputs.shape=[-1, None, 512]
         embeddings = tf.get_variable('embedding_matrix', [self.config.num_classes, self.config.hidden_dim])
         embedding_inputs = tf.nn.embedding_lookup(embeddings, tf.reshape(self._x,[self.config.batch_size*self.config.num_proposals,-1]))
 
         # LSTM Layer
-        lstm_inputs = tf.concat(values=[feature_inputs, embedding_inputs],axis=1) 
+        feature_inputs = tf.tile(input=feature_inputs,multiples=[1,self.config.num_steps,1])
+        lstm_inputs = tf.concat(values=[feature_inputs, embedding_inputs],axis=2) # stack --> shape=[-1, None, 512*2]
+        print(lstm_inputs.get_shape().as_list())
+        #lstm_inputs = tf.concat(values=[feature_inputs, embedding_inputs],axis=1) 
         lstm_cell = tf.nn.rnn_cell.LSTMCell(num_units=self.config.hidden_dim,state_is_tuple=True)
         lstm_cells = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * self.config.num_layers, state_is_tuple=True)
         initial_state = lstm_cells.zero_state(self.config.batch_size*self.config.num_proposals, tf.float32)
-        lstm_outputs, final_state = tf.nn.dynamic_rnn(cell=lstm_cells,inputs=lstm_inputs,initial_state=initial_state)                                                                          
+        lstm_outputs, final_state = tf.nn.dynamic_rnn(cell=lstm_cells,inputs=lstm_inputs,initial_state=initial_state)
+        print(lstm_outputs.get_shape().as_list())
         logits = tf.layers.dense(inputs=tf.reshape(lstm_outputs,[-1,self.config.hidden_dim]),units=self.config.num_classes)
         predictions = tf.argmax(logits,1)
-        
         # End Captioning Module
         # -----------------------
-        
-        
-        #logits = tf.reshape(logits, [-1,self.config.num_proposals,self.config.num_steps+1,self.config.num_classes])
-
 
         # Predictions
         self.logits = tf.reshape(logits, [self.config.batch_size,self.config.num_proposals,-1,self.config.num_classes])
@@ -163,7 +141,7 @@ class SPJ(object):
         batchsize = H.shape[0]
         assert (batchsize == self.config.batch_size),"batch sizes do not match!"
         x = np.ones([batchsize, self.config.num_proposals, 1])*2 # b'<sta>': 2
-        y = np.ones([batchsize, self.config.num_proposals,2,self.config.num_classes])
+        y = np.ones([batchsize, self.config.num_proposals,1,self.config.num_classes])
         while (x.shape[2] - 1) < self.config.num_steps: 
             feed = {self._H: H,
                     self._Ipast: Ipast,
@@ -175,6 +153,6 @@ class SPJ(object):
             next_x = predictions[:,:,-1]
             next_x = np.expand_dims(next_x, axis=2)
             x = np.concatenate((x,next_x),axis=2)
-            y = np.ones([batchsize, self.config.num_proposals,x.shape[2]+1,self.config.num_classes])
+            y = np.ones([batchsize, self.config.num_proposals,x.shape[2],self.config.num_classes])
         return predictions, labels
     
